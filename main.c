ce3206e985c8a279a9c92cdc3f64b23fef8611cf
Alvaro Prieto <apg88zx@gmail.com>
2015-10-09 22:02:52 -0700

    Test blinky program for when boards get back

--- main.c
+++ main.c
@@ -4,7 +4,47 @@
 #include "stm32l1xx_conf.h"
 #include "stm32l1xx.h"
 
+#define BLINK_DELAY_MS	(500)
+
+volatile uint32_t tickMs = 0;
+
+void init() {
+
+	// ---------- SysTick timer -------- //
+	if (SysTick_Config(SystemCoreClock / 1000)) {
+		// Capture error
+		while (1){};
+	}
+
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	GPIO_Init(GPIOA, &(GPIO_InitTypeDef){GPIO_Pin_15, GPIO_Mode_OUT, GPIO_OType_PP, GPIO_Speed_2MHz, GPIO_PuPd_NOPULL});
+}
+
 int main(void) {
+	uint32_t nextBlink;
+	uint32_t blinkState = 0;
+	init();
+
+	nextBlink = tickMs + BLINK_DELAY_MS;
+	for(;;) {
+
+		if(tickMs > nextBlink) {
+			nextBlink = tickMs + BLINK_DELAY_MS;
+			if(blinkState) {
+				GPIO_SetBits(GPIOA, GPIO_Pin_15);
+			} else {
+				GPIO_ResetBits(GPIOA, GPIO_Pin_15);
+			}
+			blinkState ^= 1;
+		}
+
+		__WFI();
+	}
 
 	return 0;
+}
+
+void SysTick_Handler(void)
+{
+	tickMs++;
 }
